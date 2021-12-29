16d4ae377f7ddf782ecc74535a2675c7f6837aa5
Joseph Galvin <jwgalvin@gmail.com>
2021-12-29 14:27:28 -0700

    feat: test queue at start

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,31 +1,43 @@
 require 'csv'
 
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
-end
-puts "Event Reporter has launced"
+class Event_Reporter
+  attr_reader :queue, :csv_file
 
-@queue = []
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @queue = []
+  end
 
-def queue_clear(queue)
-  @queue = queue.clear
-  puts "Your search queue has been cleared."
-end
 
-def find(input)
-  #to be defined
-end
+  def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5,"0")[0..4]
+  end
 
-sleep (.5)
 
-def load(file = "full_event_attendees.csv")
-  file_load = CSV.open file, headers: true, header_converters: :symbol
-end
+  def queue_clear
+    @queue = queue.clear
+    puts "Your search queue has been cleared."
+  end
 
-#contents = CSV.open "full_event_attendees.csv", headers: true, header_converters: :symbol
+  def find(attribute, value)
+    @queue.queue_clear
+    @contents.filter do |row|
+      @queue << row if value[attribute.to_sym] == value
+    end
+    puts queue
+    @queue
+  end
 
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  def load(file = "full_event_attendees.csv")
+    file_load = CSV.open file, headers: true, header_converters: :symbol
+    puts "#{file} has been loaded."
+  end
+
+
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   puts "#{name} #{zipcode}"
+# end
+
 end
--- spec/event_reporter_spec.rb
+++ spec/event_reporter_spec.rb
@@ -1,9 +1,14 @@
 require 'pry'
+require 'pry'
 require './lib/event_reporter'
 require 'Rspec'
 
-Rspec.describe Event_Reporter do
-  it "tests things" do
+describe Event_Reporter do
+  it "event_reporter returns an empty array when intialized" do
+    events = Event_Reporter.new("full_event_attendees.csv")
 
+    expect(events).to be_a(Event_Reporter)
+    expect(events.queue).to eq([])
   end
+
 end
